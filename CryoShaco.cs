034eacbbcdb01d4c782215f55ccce80dc1248e33
Tyler Harris <tyler.harris72195@gmail.com>
2017-08-04 16:37:37 -0600

    CryoShaco

--- CryoShaco.cs
+++ CryoShaco.cs
@@ -21,7 +21,29 @@ namespace CryoShaco
         public static Menu CS_menu = new Menu("cshaco", "CryoShaco", true);
         public static Orbwalker CS_orbwalker = new Orbwalker();
         public static Obj_AI_Hero CS_player = ObjectManager.GetLocalPlayer();
-        public static Spell _q, _w, _e, _r;
+        public static Spell _q, _w, _e, _r, smite;
+        public static int CS_smiteDmg
+        {
+            get
+            {
+                int[] damage = new int[] { 390, 410, 430, 450, 480, 510, 540, 570, 600, 640, 680, 720, 760, 800, 850, 900, 950, 1000 };
+
+                return damage[CS_player.Level - 1];
+            }
+        }
+        private static int CS_smiteChampDmg
+        {
+            get
+            {
+                int[] damage = new int[] { 28, 36, 44, 52, 60, 68, 76, 84, 92, 100, 108, 116, 124, 132, 140, 148, 156, 166 };
+
+                return damage[CS_player.Level - 1];
+            }
+        }
+        private static readonly string[] smiteDragons = { "SRU_Dragon_Water", "SRU_Dragon_Fire", "SRU_Dragon_Earth", "SRU_Dragon_Air", "SRU_Dragon_Elder" };
+        private static readonly string[] smiteEMobs = { "SRU_Baron", "SRU_RiftHerald" };
+        private static readonly string[] smiteBuffs = { "SRU_Blue", "SRU_Red" };
+        private static readonly string[] smiteSMobs = { "SRU_Murkwolf", "SRU_Krug", "SRU_Razorbeak", "SRU_Gromp", "SRU_Crab" };
 
         public CryoShaco()
         {
@@ -29,6 +51,8 @@ namespace CryoShaco
             _w = new Spell(SpellSlot.W, 425);
             _e = new Spell(SpellSlot.E, 625);
             _r = new Spell(SpellSlot.R);
+            var smiteSlot = CS_player.SpellBook.Spells.FirstOrDefault(x => x.SpellData.Name.ToLower().Contains("smite"));
+            if (smiteSlot != null) smite = new Spell(smiteSlot.Slot, 700);
 
             CS_orbwalker.Attach(CS_menu);
 
@@ -52,6 +76,29 @@ namespace CryoShaco
             harass.Add(new MenuSliderBool("usew", "Use W / mana >= x%", true, 60, 0, 99));
             harass.Add(new MenuSliderBool("usee", "Use E / mana >= x%", true, 60, 0, 99));
             CS_menu.Add(harass);
+
+            var smiteMenu = new Menu("cssmite", "Smite Usage");
+            smiteMenu.Add(new MenuKeyBind("autosmitekey", "Toggle Smite", KeyCode.G, KeybindType.Toggle, true));
+
+            var smiteEliteMob = new Menu("cssmiteelite", "Elite Mobs");
+            smiteEliteMob.Add(new MenuBool("SRU_Baron", "Baron", true));
+            smiteEliteMob.Add(new MenuBool("smitedragon", "Dragon", true));
+            smiteEliteMob.Add(new MenuBool("SRU_RiftHerald", "Rift Herald", true));
+            smiteMenu.Add(smiteEliteMob);
+
+            var smiteBuffs = new Menu("cssmitebuffs", "Buffs");
+            smiteBuffs.Add(new MenuBool("SRU_Red", "Red", true));
+            smiteBuffs.Add(new MenuBool("SRU_Blue", "Blue", true));
+            smiteMenu.Add(smiteBuffs);
+
+            var smiteSmalls = new Menu("cssmitesmalls", "Small");
+            smiteSmalls.Add(new MenuBool("SRU_Murkwolf", "Wolves", false));
+            smiteSmalls.Add(new MenuBool("SRU_Krug", "Krugs", false));
+            smiteSmalls.Add(new MenuBool("SRU_Razorbeak", "Razorbeaks", false));
+            smiteSmalls.Add(new MenuBool("SRU_Gromp", "Gromp", false));
+            smiteSmalls.Add(new MenuBool("Sru_Crab", "Crab", false));
+            smiteMenu.Add(smiteSmalls);
+            CS_menu.Add(smiteMenu);
 
             var lasthit = new MenuSliderBool("cslasthitemana", "Last hit E / if mana >= x%", true, 50, 0, 99);
             CS_menu.Add(lasthit);
@@ -82,6 +129,20 @@ namespace CryoShaco
                 case OrbwalkingMode.Mixed:
                     CS_DoHarass();
                     break;
+            }
+        }
+
+        private static void CS_AutoSmite()
+        {
+            if (smite != null && CS_menu["cssmite"]["autosmitekey"].Enabled)
+            {
+                foreach (var minion in GameObjects.Jungle.Where(x => x.IsValidTarget(smite.Range) && x.Health <= CS_smiteDmg && x.IsValidSpellTarget()))
+                {
+                    if (smiteEMobs.Contains(minion.UnitSkinName) && CS_menu["cssmiteelite"][minion.UnitSkinName].Enabled) smite.Cast(minion);
+                    if (smiteBuffs.Contains(minion.UnitSkinName) && CS_menu["cssmitebuffs"][minion.UnitSkinName].Enabled) smite.Cast(minion);
+                    if (smiteSMobs.Contains(minion.UnitSkinName) && CS_menu["cssmitesmalls"][minion.UnitSkinName].Enabled) smite.Cast(minion);
+                    if (smiteDragons.Contains(minion.UnitSkinName) && CS_menu["cssmiteelite"]["smitedragon"].Enabled) smite.Cast(minion);
+                }
             }
         }
 
